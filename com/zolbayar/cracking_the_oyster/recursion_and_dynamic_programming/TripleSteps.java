5dcbc1b489eef7888a9bebce7abfc47e0e8e13d6
Zolbayar <zolbayarq@gmail.com>
2018-10-15 08:08:03 +0800

    Added insight

--- com/zolbayar/cracking_the_oyster/recursion_and_dynamic_programming/TripleSteps.java
+++ com/zolbayar/cracking_the_oyster/recursion_and_dynamic_programming/TripleSteps.java
@@ -3,6 +3,8 @@ package com.zolbayar.cracking_the_oyster.recursion_and_dynamic_programming;
 /*
     Purpose: There are n steps. A child can hop on those in 1 step, 2 steps or 3 steps.
             Find how many possible ways the child can run up the stairs.
+
+    Insights: - Approach top-down. What's the last step child make? ~ The step where n becomes 0?
  */
 public class TripleSteps {
 
@@ -10,17 +12,9 @@ public class TripleSteps {
 
     public int countStepsBF(int n){
 
-        if(n <= 0){
-            return 0;
-        }
-
-        if(n == 1){
-            return 1;
-        }
-
-        if(n == 2){
-            return 2;
-        }
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(n == 1) return 1;
 
         int result = 0;
         for(int i = 0; i < stepOptions.length; i++){
