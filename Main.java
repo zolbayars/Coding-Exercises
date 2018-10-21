7f6ccaf6e4cf1ff92439df90f46671353a60ef5d
Zolbayar <zolbayarq@gmail.com>
2018-10-21 11:25:57 +0800

    "Robot in Grid" dynamic programming challenge - insights and changes from the book

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ import com.zolbayar.cracking_the_oyster.recursion_and_dynamic_programming.Fibona
 import com.zolbayar.cracking_the_oyster.recursion_and_dynamic_programming.RobotInGrid;
 import com.zolbayar.cracking_the_oyster.recursion_and_dynamic_programming.TripleSteps;
 
+import java.awt.*;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
@@ -16,8 +19,24 @@ public class Main {
 //        TripleSteps tripleSteps = new TripleSteps();
 //        int result = tripleSteps.countSteps(35);
 
+        boolean[][] booleanGrid = new boolean[5][7];
+        boolean[] booleanCol = new boolean[7];
+        Arrays.fill(booleanCol, true);
+        Arrays.fill(booleanGrid, booleanCol);
+
+//        booleanGrid[2][2] = false;
+
         RobotInGrid robotInGrid = new RobotInGrid();
-        robotInGrid.moveRobotFromTop();
+        ArrayList<Point> path = robotInGrid.getPath(booleanGrid);
+
+        if(path == null){
+            System.out.println("There is no path");
+        }else{
+            System.out.println("Here's the path:");
+            for (int i = 0; i < path.size(); i++) {
+                System.out.println(path.get(i).toString());
+            }
+        }
 
 //        System.out.println("Result: " + result);
 
--- com/zolbayar/cracking_the_oyster/recursion_and_dynamic_programming/RobotInGrid.java
+++ com/zolbayar/cracking_the_oyster/recursion_and_dynamic_programming/RobotInGrid.java
@@ -1,17 +1,31 @@
 package com.zolbayar.cracking_the_oyster.recursion_and_dynamic_programming;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /*
     Purpose: A robot can only move in right and down direction in a grid with c columns and r rows.
             Some cells are off-limit for that robot
 
     My Solution: grid should be 2d array with 0s filled in. The off limit cells are filled with -1s.
                  The cells where robot moved should be filled with 1.
+
+    Insights: - Really try to understand the problem statement! (The robot is not able to move
+                down AND right in a same move)
+              - Check if there is a path
+              - The base intuition was right.
+              - If the value is a flag, use boolean
+              - Always think about it from the end! What's the second last spot look like? etc.
  */
 public class RobotInGrid {
 
     private int[][] grid;
     private int counter = 0;
 
+
     public RobotInGrid(){
+        // Not used
         this.grid = new int[5][7];
         for (int i = 0; i < this.grid.length; i++) {
             for (int j = 0; j < this.grid[i].length; j++) {
@@ -21,14 +35,40 @@ public class RobotInGrid {
 
         // Starting point for the robot
         this.grid[0][0] = 1;
-
-
     }
 
     public RobotInGrid(int[][] grid){
         this.grid = grid;
     }
 
+    public ArrayList<Point> getPath(boolean[][] gridBool){
+        ArrayList<Point> path = new ArrayList<>();
+        if(getPath(gridBool, gridBool.length - 1, gridBool[0].length - 1, path)){
+            return path;
+        }
+
+        return null;
+    }
+
+    private boolean getPath(boolean[][] grid, int row, int col, ArrayList<Point> path){
+
+        System.out.println(row + " - " + col);
+        // Check if it's out of range
+        if(row < 0 || col < 0 || !grid[row][col]){
+            return false;
+        }
+
+        boolean isOriginalPoint = (row == 0) && (col == 0);
+
+        if(isOriginalPoint || getPath(grid, row - 1, col, path) || getPath(grid, row, col - 1, path)){
+            path.add(new Point(row, col));
+            return true;
+        }
+
+        return false;
+    }
+
+    // My way - Not correct
     public void moveRobotFromTop(){
         moveToRightMostBottom(this.grid, 0, 0);
     }
