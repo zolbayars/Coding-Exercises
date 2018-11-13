6ff4823b3d1532caeed7be3b8ef339b481b3a4f1
Zolbayar <zolbayarq@gmail.com>
2018-11-13 08:01:05 +0800

    "Magic Index" dynamic programming challenge - Towers of Hanoi - Trying to build a recursive solution

--- com/zolbayar/cracking_the_oyster/recursion_and_dynamic_programming/TowersOfHanoi.java
+++ com/zolbayar/cracking_the_oyster/recursion_and_dynamic_programming/TowersOfHanoi.java
@@ -3,6 +3,9 @@ package com.zolbayar.cracking_the_oyster.recursion_and_dynamic_programming;
 /*
     Purpose: A classic problem. There are 3 towers and N number of disks ordered by their sizes.
         Move the first tower to the 3rd tower using stacks
+
+    Insight: A disk cannot be placed on a smaller disk! Read the description carefully!
+            Base case and Build!
  */
 
 import java.util.Stack;
@@ -27,13 +30,44 @@ public class TowersOfHanoi {
     }
 
     public void move(){
-        while(!this.tower1.empty()){
-            this.tower2.push(this.tower1.pop());
-        }
-        while(!this.tower2.empty()){
-            this.tower3.push(this.tower2.pop());
+        Stack<Integer> result = move(this.tower1, this.tower2, this.tower3);
+    }
+
+    public Stack<Integer> move(Stack<Integer> tower1, Stack<Integer> tower2, Stack<Integer> tower3){
+
+        if(tower1.size() == 1){
+            tower3.push(tower1.pop());
         }
 
-        System.out.println("tower3 = " + tower3.peek());
+        if(tower1.size() == 2){
+            tower2.push(tower1.pop());
+            tower2.push(tower1.pop());
+            tower3.push(tower2.pop());
+            tower3.push(tower2.pop());
+        }
+
+        if(tower1.size() == 3){
+            tower3.push(tower1.pop());
+            tower2.push(tower1.pop());
+            tower2.push(tower3.pop());
+            tower3.push(tower1.pop());
+            tower3.push(tower2.pop());
+            tower3.push(tower2.pop());
+        }
+
+        if(tower1.size() == 4){
+            tower2.push(tower1.pop());
+            tower3.push(tower1.pop());
+            tower3.push(tower2.pop());
+            tower2.push(tower1.pop());
+            tower2.push(tower3.pop());
+            tower2.push(tower3.pop());
+            tower3.push(tower1.pop());
+            tower3.push(tower2.pop());
+            tower3.push(tower2.pop());
+            tower3.push(tower2.pop());
+        }
+
+        return null;
     }
 }
